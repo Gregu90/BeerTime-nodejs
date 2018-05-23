d1129b9415d0e64c5661b3c719e41edd23042764
Grzegorz Palka <grzegorz.palka@senfino.com>
2018-05-23 15:46:08 +0200

    Address when logged in

--- configuration/auth.js
+++ configuration/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '448801155578053', // your App ID
         'clientSecret'  : 'b8aafbf70be3a0a59411aea1a7197dfe', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
+        'callbackURL'   : 'https://beertime-app.herokuapp.com/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
     },
@@ -11,13 +11,13 @@ module.exports = {
     'twitterAuth' : {
         'consumerKey'       : 'your-consumer-key-here',
         'consumerSecret'    : 'your-client-secret-here',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'       : 'https://beertime-app.herokuapp.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : 'your-secret-clientID-here',
         'clientSecret'  : 'your-client-secret-here',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'   : 'https://beertime-app.herokuapp.com/auth/google/callback'
     }
 
 };
